42c5e8562ef3f67cf0ae3e29b57c3778d963f806
Bryan Jensen <bryanawjensen@gmail.com>
2014-11-20 16:04:32 -0500

    Adding saved sets of nets

--- path-1/source/driver.cpp
+++ path-1/source/driver.cpp
@@ -11,13 +11,25 @@ int main(int argc, char** argv) {
 
     std::vector<std::string> args(argv, argv+argc);
     for (size_t i = 1; i < args.size(); ++i) {
-        if (args[i] == "-d") {
-            cout << "Enabling game controller debug mode" << endl;
-            GameController::debug = true;
-        }
-        else if (args[i] == "-n") {
-            cout << "Enabling net testing mode" << endl;
-            gc.setTestingNets(true);
+        // if (args[i] == "-d") {
+        //     cout << "Enabling game controller debug mode" << endl;
+        //     GameController::debug = true;
+        // }
+        // else if (args[i] == "-n") {
+        //     cout << "Enabling net testing mode" << endl;
+        //     gc.setTestingNets(true);
+        // }
+        if (args[i].length() > 1 && args[i][0] == '-') {
+            switch (args[i][1]) {
+                case 'd':
+                    cout << "Enabling game controller debug mode" << endl;
+                    GameController::debug = true;
+                    break;
+                case 'n':
+                    cout << "Enabling net testing mode" << endl;
+                    gc.setTestingNets(true);
+                    break;
+            }
         }
     }
 
