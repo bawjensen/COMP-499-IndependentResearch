1450d16dc7315e4af3f79254c16a04e4391219e3
Bryan Jensen <bryanawjensen@gmail.com>
2014-11-20 16:11:07 -0500

    Adding better support for flags, using switch

--- path-1/source/driver.cpp
+++ path-1/source/driver.cpp
@@ -7,18 +7,19 @@ using namespace std;
 bool debug;
 
 int main(int argc, char** argv) {
+    // Initialize game controller
     GameController gc;
 
+    // Default values
+    gc.setNumGenerations(10);
+    gc.setNumNets(10);
+    gc.setNumGamesPerNet(10);
+
+    // Create vector from argv
     std::vector<std::string> args(argv, argv+argc);
+
+    // Loop for flags
     for (size_t i = 1; i < args.size(); ++i) {
-        // if (args[i] == "-d") {
-        //     cout << "Enabling game controller debug mode" << endl;
-        //     GameController::debug = true;
-        // }
-        // else if (args[i] == "-n") {
-        //     cout << "Enabling net testing mode" << endl;
-        //     gc.setTestingNets(true);
-        // }
         if (args[i].length() > 1 && args[i][0] == '-') {
             switch (args[i][1]) {
                 case 'd':
@@ -31,7 +32,25 @@ int main(int argc, char** argv) {
                     break;
             }
         }
+        else {
+            if ((i > 0) && (args[i-1].length() > 0) && (args[i-1][0] != '-')) {
+                cout << "Ended flags, grabbing cmd line args" << endl;
+                break; // Finished with flags, time to grab cmd line args
+            }
+        }
     }
+
+    // if (i+3 >= args.size()) {
+    //     cout << "Warning: Too few parameters, defaulting to 10 generations, 10 nets and 10 games" << endl;
+    //     gc.setNumGenerations(10);
+    //     gc.setNumNets(10);
+    //     gc.setNumGamesPerNet(10);
+    // }
+    // else {
+    //     gc.setNumGenerations(args[i]);
+    //     gc.setNumNets(args[i+1]);
+    //     gc.setNumGamesPerNet(args[i+2]);
+    // }
 
     gc.start();
 
