f54bb9b19b825059880d28c91800255925e3c870
Bryan Jensen <bryanawjensen@gmail.com>
2014-12-09 12:19:02 -0500

    Removing old minecraft work, and shifting 2048 work into main folder

--- path-0/forge/src/main/java/bawjensen/minecraft/hw/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package bawjensen.minecraft.hw;
-
-public class CommonProxy {
-
-        // Client stuff
-        public void registerRenderers() {
-                // Nothing here as the server doesn't render graphics or entities!
-        }
-}
