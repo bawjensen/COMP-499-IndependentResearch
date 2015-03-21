fdab6e7a51a6918b5f378df862c47865f354f8e1
Bryan Jensen <bryanawjensen@gmail.com>
2015-03-21 15:39:31 -0400

    Cleaning up commented out code

--- neural-nets/headers/GameController.h
+++ neural-nets/headers/GameController.h
@@ -22,12 +22,7 @@ private:
     std::ofstream logFile;
     std::streambuf *coutbuf; // old buf location
 
-    // int numNets;
-    // int numGenerations;
-    // int numGamesPerNet;
-    // int netHiddenLayerSize;
     EvalMode mode;
-    // int treeDepth;
 
 public:
     static bool debug;
@@ -37,30 +32,17 @@ public:
     GameController(int numGenerations, int numNets, int numGamesPerNet, int netHiddenLayerSize, char chMode, int treeDepth);
 
     void reset();
-    // void initialize(int numGenerations, int numNets, int numGamesPerNet, int netHiddenLayerSize, char chMode, int treeDepth);
 
     void setDebug(bool debug) { this->debug = debug; };
-    // void setNumNets(int numNets) { this->numNets = numNets; };
-    // void setNumGenerations(int numGenerations) { this->numGenerations = numGenerations; };
-    // void setNumGamesPerNet(int numGamesPerNet) { this->numGamesPerNet = numGamesPerNet; };
-    // void setNetHiddenLayerSize(int netHiddenLayerSize) { this->netHiddenLayerSize = netHiddenLayerSize; };
     void setEvaluationMode(char chMode);
 
     bool getDebug() { return this->debug; };
-    // int getNumNets() { return this->numNets; };
-    // int getNumGenerations() { return this->numGenerations; };
-    // int getNumGamesPerNet() { return this->numGamesPerNet; };
-    // int getNetHiddenLayerSize() { return this->netHiddenLayerSize; };
 
     bool gameEnded();
-    // void start();
-    // void start(int numGenerations, int numNets, int numGamesPerNet, int netHiddenLayerSize, char chMode, int treeDepth);
 
-    // void testNets();
     void runTesting();
     void runTraining(int numGenerations, int numNets, int numGamesPerNet, int netHiddenLayerSize, char chMode, int treeDepth);
 
-    // void runGame();
     int runGameWithNet(NeuralNet& net);
     
     void redirectOutputTo(std::string logFilePath);
@@ -68,5 +50,4 @@ public:
     void saveNets(int numNets);
     bool handleCommand(char& input);
     std::pair<bool, int> handleCommand(const int& direction);
-    // bool movesAvailable();
 };
